47737503244b8f3464000db1b0912e0742402ec3
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-02-27 19:31:46 +0900

    스태틱 변수 추가

---
+++ WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
@@ -0,0 +1,8 @@
+package com.example.wordmaster.define;
+
+public class Define {
+    public static final int HOME_FRAGMENT = 0;
+    public static final int DICTIONARY_FRAGMENT = 1;
+    public static final int MY_INFO_FRAGMENT = 2;
+
+}
