df4aa5b8d3479c4b82e31e521f6e9423e6abb235
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-02 22:56:28 +0900

    로그인 이벤트 아직 미완성

--- WordMaster/app/src/main/java/com/example/wordmaster/activities/LoginActivity.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/activities/LoginActivity.java
@@ -3,19 +3,36 @@ package com.example.wordmaster.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.example.wordmaster.R;
+import com.example.wordmaster.callback.SessionCallback;
 import com.example.wordmaster.databinding.ActivityLoginBinding;
 import com.example.wordmaster.dialog.bottomsheet.LoginBottomSheetDialog;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding mb;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient googleApiClient;
+    private static final int RESULT_SIGN_GOOGLE = 100;
     private static final String TAG = "LoginActivity";
 
     @Override
@@ -24,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         mb = ActivityLoginBinding.inflate(getLayoutInflater());
         View root = mb.getRoot();
         setContentView(root);
+        getHashKey();
         Log.e(TAG, "onCreate: " );
         init();
 
@@ -36,6 +54,20 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void init(){
+        mb.btnGoogleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mb.kakaoLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Session session = Session.getCurrentSession();
+                session.addCallback(new SessionCallback());
+                session.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
+            }
+        });
         mb.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,5 +98,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.e(TAG, "onDestroy: " );
+    }private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
     }
 }
