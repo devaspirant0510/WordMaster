3e1d9b7c7e0a3d88bca70bc3859e2be4b0c09a31
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-02 23:00:17 +0900

    바텀시트 다이얼로그에서 보낸 값 받아옴

--- WordMaster/app/src/main/java/com/example/wordmaster/fragment/DictionaryFragment.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/fragment/DictionaryFragment.java
@@ -9,24 +9,34 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.wordmaster.activities.MainActivity;
 import com.example.wordmaster.adapter.DictionaryListAdapter;
+import com.example.wordmaster.callback.BottomSheetCallBack;
 import com.example.wordmaster.data.DictionaryListItem;
 import com.example.wordmaster.databinding.FragmentDictionaryBinding;
+import com.example.wordmaster.dialog.bottomsheet.CreateDictionarySheetDialog;
 
 
-public class DictionaryFragment extends Fragment {
+public class DictionaryFragment extends Fragment implements BottomSheetCallBack {
     private FragmentDictionaryBinding mb;
     private static final String TAG = "DictionaryFragment";
+    private CreateDictionarySheetDialog dialog;
+    private MainActivity activity;
+    private DictionaryListAdapter adapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity = (MainActivity)getActivity();
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        activity.BottomSheetCallBack(this);
         mb = FragmentDictionaryBinding.inflate(getLayoutInflater());
         init();
 
@@ -36,11 +46,28 @@ public class DictionaryFragment extends Fragment {
     }
 
     private void init() {
-        DictionaryListAdapter adapter = new DictionaryListAdapter(getContext());
-        adapter.addItem(new DictionaryListItem("s","s","s","s",1));
-        adapter.addItem(new DictionaryListItem("s","s","s","s",1));
+         adapter = new DictionaryListAdapter(getContext());
         Log.e(TAG, "init: "+adapter.dictList);
         mb.dictionaryList.setAdapter(adapter);
+        mb.createDictionary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 dialog = new CreateDictionarySheetDialog(activity);
+
+                if (getFragmentManager()!=null){
+                    dialog.show(getFragmentManager(),"dd");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void createDialogGetData(String title, int count, String description, String hashTag, int groupType) {
+        Log.e(TAG,title);
+        adapter.addItem(new DictionaryListItem(title,String.valueOf(count),description,hashTag,1));
+        adapter.notifyDataSetChanged();
 
     }
+
+
 }
