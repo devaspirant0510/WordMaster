b8e77d9337d7a019b10f9f25b971a9d8db20a458
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-04 19:24:46 +0900

    단어장 세부정보 리사이클러뷰 아이템 데이터

---
+++ WordMaster/app/src/main/java/com/example/wordmaster/data/recycler/DictionaryWordItem.java
@@ -0,0 +1,27 @@
+package com.example.wordmaster.data.recycler;
+
+public class DictionaryWordItem {
+    String kor;
+    String eng;
+
+    public DictionaryWordItem(String kor, String eng) {
+        this.kor = kor;
+        this.eng = eng;
+    }
+
+    public String getKor() {
+        return kor;
+    }
+
+    public void setKor(String kor) {
+        this.kor = kor;
+    }
+
+    public String getEng() {
+        return eng;
+    }
+
+    public void setEng(String eng) {
+        this.eng = eng;
+    }
+}
