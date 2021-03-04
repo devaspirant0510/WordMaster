fc35417ae5b2ffebff280febf7ada30d2159ced3
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-04 19:14:32 +0900

    바텀시트에서 넘기는 값중 필요없는값 제외

--- WordMaster/app/src/main/java/com/example/wordmaster/callback/BottomSheetCallBack.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/callback/BottomSheetCallBack.java
@@ -1,5 +1,5 @@
 package com.example.wordmaster.callback;
 
 public interface BottomSheetCallBack {
-    void createDialogGetData(String title,int count,String description,String hashTag,int groupType);
+    void createDialogGetData(String title,int count,String description,String hashTag,String DictOption);
 }
