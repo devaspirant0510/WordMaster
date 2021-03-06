fab8942133ca0801369b25e6f6f0cdce58199065
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-07 01:19:16 +0900

    스태틱 변수 추가 (프레그먼트 상수)

--- WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
@@ -1,10 +1,14 @@
 package com.example.wordmaster.define;
 
 public class Define {
+    //바텀네비게이션 프레그먼트
     public static final int HOME_FRAGMENT = 0;
     public static final int DICTIONARY_FRAGMENT = 1;
-    public static final int MY_INFO_FRAGMENT = 2;
-    public static final int DICTIONARY_INFO_FRAGMENT = 3;
+    public static final int TEST_FRAGMENT = 2;
+    public static final int SEARCH_FRAGMENT = 3;
+    public static final int MY_INFO_FRAGMENT = 4;
+
+    public static final int DICTIONARY_INFO_FRAGMENT = 5;
 
     public static final int PUBLIC = 1;
     public static final int PRIVATE = 2;
