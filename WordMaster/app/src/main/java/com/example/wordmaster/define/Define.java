41fe97669bfef574e5c93dbe862cda05765c2051
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-08 23:53:21 +0900

    스태틱 변수 추가 (프레그먼트 아이디)

--- WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
@@ -8,8 +8,13 @@ public class Define {
     public static final int SEARCH_FRAGMENT = 3;
     public static final int MY_INFO_FRAGMENT = 4;
 
+    // 메인액티비티 뷰 상태 (프레그먼트인지 뷰페이저인지)
+    public static final int SHOW_FRAGMENT = 1;
+    public static final int SHOW_VIEW_PAGER = 2;
+    //그외 프레그먼트
     public static final int DICTIONARY_INFO_FRAGMENT = 5;
 
+    // 단어장 공개여부
     public static final int PUBLIC = 1;
     public static final int PRIVATE = 2;
     public static final int ONLY_ME = 3;
