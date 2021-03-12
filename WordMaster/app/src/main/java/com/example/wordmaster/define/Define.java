ca123a2c5523dc55442c4023067f3843f448e1fe
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-12 14:05:37 +0900

    스태틱 변수 추가

--- WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/define/Define.java
@@ -13,6 +13,7 @@ public class Define {
     public static final int SHOW_VIEW_PAGER = 2;
     //그외 프레그먼트
     public static final int DICTIONARY_INFO_FRAGMENT = 5;
+    public static final int TESTING_FRAGMENT = 6;
 
     // 단어장 공개여부
     public static final int PUBLIC = 1;
