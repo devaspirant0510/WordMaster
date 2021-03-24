b9ce526620f1f15b39dc9272b32020b448e89a42
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-24 18:02:32 +0900

    단어장 불러와서 테스트 보여줌

--- WordMaster/app/src/main/java/com/example/wordmaster/fragment/TestFragment.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/fragment/TestFragment.java
@@ -5,23 +5,30 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.wordmaster.data.recycler.DictionaryWordItem;
 import com.example.wordmaster.databinding.FragmentTestBinding;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class TestFragment extends Fragment{
     private FragmentTestBinding mb;
     private int dictMaxCount,rgDictType,rgOption;
     private String dictTitle,dictHostName,limitTime;
     private static String TAG = "TestFragment";
+    private int currentIdx=0;
+    private ArrayList<DictionaryWordItem> testList = new ArrayList<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,15 +50,81 @@ public class TestFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         init();
         readDBListItem();
+        setTestList();
+
         return mb.getRoot();
     }
+    public void showWord(int pos){
+        DictionaryWordItem item = testList.get(pos);
+        String kor = item.getKor();
+        String eng = item.getEng();
+        mb.tvWordQuestion.setText(eng);
+    }
+    private void setTestList() {
+        mb.ibNextWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentIdx+1<dictMaxCount){
+                    currentIdx+=1;
+                    showWord(currentIdx);
+                }
+                else{
+                    mb.ibNextWord.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+        mb.ibPreviousWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentIdx>0){
+                    currentIdx-=1;
+                    showWord(currentIdx);
+                }
+                else{
+                    mb.ibPreviousWord.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+    }
+
     public void readDBListItem(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference(dictHostName);
-        myRef.child(dictTitle).child("list").addListenerForSingleValueEvent(new ValueEventListener() {
+        myRef.child(dictTitle).child("list").addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.e(TAG, "onDataChange: "+snapshot.getValue());
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Log.e(TAG, "onChildAdded: "+snapshot.getValue() );
+                String item = snapshot.getValue().toString();
+                item = item.replaceAll("[}{]","");
+                String[] splitWord = item.split(",");
+                String strEng = splitWord[0].split("=")[1];
+                String strKor = splitWord[1].split("=")[1];
+                testList.add(new DictionaryWordItem(strEng,strKor));
+                Log.e(TAG, "onChildChanged: "+strEng );
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Log.e(TAG, "onChildAdded: "+snapshot.getValue() );
+                String item = snapshot.getValue().toString();
+                item = item.replaceAll("[}{]","");
+                String[] splitWord = item.split(",");
+                String strEng = splitWord[0].split("=")[1];
+                String strKor = splitWord[1].split("=")[1];
+                Log.e(TAG, "onChildChanged: "+strEng );
+                testList.add(new DictionaryWordItem(strEng,strKor));
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
             }
 
             @Override
