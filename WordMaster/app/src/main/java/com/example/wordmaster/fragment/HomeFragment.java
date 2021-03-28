1f127d0b3f802fb1e8c86e697f12410dc5895ced
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-28 20:09:41 +0900

    리사이클러뷰 어댑터 연결

--- WordMaster/app/src/main/java/com/example/wordmaster/fragment/HomeFragment.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.wordmaster.fragment;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.wordmaster.R;
+import com.example.wordmaster.adapter.RankingAdapter;
+import com.example.wordmaster.data.recycler.RankingItem;
 import com.example.wordmaster.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -23,9 +27,18 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mb = FragmentHomeBinding.inflate(getLayoutInflater());
-        View view = mb.getRoot();
-        return view;
+        init();
+        return mb.getRoot();
 
+
+    }
+
+    private void init() {
+        RankingAdapter adapter = new RankingAdapter(getContext());
+        adapter.addItem(new RankingItem("3.14썬",1,"lsh0510","강력하다",R.drawable.ic_launcher_foreground));
+        adapter.addItem(new RankingItem("유클립트",2,"xkuq1234","유클립트 TV 구독과 좋아요",R.drawable.ic_launcher_foreground));
+        adapter.addItem(new RankingItem("황홀한 러닝커브",3,"seungho020510","딥러닝 잘해지고 싶어요",R.drawable.ic_launcher_foreground));
+        mb.rankingRecyclerView.setAdapter(adapter);
 
     }
 }
