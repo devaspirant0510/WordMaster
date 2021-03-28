e08d377c011c792f8471346c131ec21f49271b7e
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-03-28 20:15:23 +0900

    인터페이스 수정

--- WordMaster/app/src/main/java/com/example/wordmaster/callback/SendDataToActivity.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/callback/SendDataToActivity.java
@@ -1,6 +1,6 @@
 package com.example.wordmaster.callback;
 
 public interface SendDataToActivity {
-    void sendDictData(String title,String option,String description,String hashTag,int count);
+    void sendDictData(String title,String option,String hashTag,int count);
     void sendTestingData(int maxCount, String limitTime, int rgTestType, int rgTestTimeOption,String host,String title);
 }
