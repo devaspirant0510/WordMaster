173e964cc802221e098b6001853fbf4c91c5ae45
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-04-03 19:21:16 +0900

    카카오톡 로그인

--- WordMaster/app/src/main/java/com/example/wordmaster/callback/SessionCallback.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/callback/SessionCallback.java
@@ -25,41 +25,41 @@ public class SessionCallback implements ISessionCallback {
     // 사용자 정보 요청
     public void requestMe() {
         // 사용자정보 요청 결과에 대한 Callback
-        UserManagement.requestMe(new MeResponseCallback() {
-            // 세션 오픈 실패. 세션이 삭제된 경우,
-            @Override
-            public void onSessionClosed(ErrorResult errorResult) {
-                Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
-            }
-
-            // 회원이 아닌 경우,
-            @Override
-            public void onNotSignedUp() {
-                Log.e("SessionCallback :: ", "onNotSignedUp");
-            }
-
-            // 사용자정보 요청에 성공한 경우,
-            @Override
-            public void onSuccess(UserProfile userProfile) {
-
-                Log.e("SessionCallback :: ", "onSuccess");
-
-                String nickname = userProfile.getNickname();
-                String profileImagePath = userProfile.getProfileImagePath();
-                String thumnailPath = userProfile.getThumbnailImagePath();
-                long id = userProfile.getId();
-
-                Log.i("Profile : ", "nickname / " + nickname + "");
-                Log.i("Profile : ", "profileImagePath / " + profileImagePath  + "");
-                Log.i("Profile : ", "thumnailPath / " + thumnailPath + "");
-                Log.i("Profile : ", "id / " + id + "");
-            }
-
-            // 사용자 정보 요청 실패
-            @Override
-            public void onFailure(ErrorResult errorResult) {
-                Log.e("SessionCallback :: ", "onFailure : " + errorResult.getErrorMessage());
-            }
-        });
+//        UserManagement.requestMe(new MeResponseCallback() {
+//            // 세션 오픈 실패. 세션이 삭제된 경우,
+//            @Override
+//            public void onSessionClosed(ErrorResult errorResult) {
+//                Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
+//            }
+//
+//            // 회원이 아닌 경우,
+//            @Override
+//            public void onNotSignedUp() {
+//                Log.e("SessionCallback :: ", "onNotSignedUp");
+//            }
+//
+//            // 사용자정보 요청에 성공한 경우,
+//            @Override
+//            public void onSuccess(UserProfile userProfile) {
+//
+//                Log.e("SessionCallback :: ", "onSuccess");
+//
+//                String nickname = userProfile.getNickname();
+//                String profileImagePath = userProfile.getProfileImagePath();
+//                String thumnailPath = userProfile.getThumbnailImagePath();
+//                long id = userProfile.getId();
+//
+//                Log.i("Profile : ", "nickname / " + nickname + "");
+//                Log.i("Profile : ", "profileImagePath / " + profileImagePath  + "");
+//                Log.i("Profile : ", "thumnailPath / " + thumnailPath + "");
+//                Log.i("Profile : ", "id / " + id + "");
+//            }
+//
+//            // 사용자 정보 요청 실패
+//            @Override
+//            public void onFailure(ErrorResult errorResult) {
+//                Log.e("SessionCallback :: ", "onFailure : " + errorResult.getErrorMessage());
+//            }
+//        });
     }
 }
