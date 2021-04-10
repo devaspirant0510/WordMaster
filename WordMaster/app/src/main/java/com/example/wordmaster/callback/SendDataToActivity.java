24165075759b008f9f20ba20cf7f8e4054b3ef44
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-04-10 11:13:03 +0900

    콜백 메서드에 ArrayList 추가

--- WordMaster/app/src/main/java/com/example/wordmaster/callback/SendDataToActivity.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/callback/SendDataToActivity.java
@@ -1,7 +1,11 @@
 package com.example.wordmaster.callback;
 
+import com.example.wordmaster.data.recycler.DictionaryWordItem;
+
+import java.util.ArrayList;
+
 public interface SendDataToActivity {
     void sendDictData(String title,String option,String hashTag,int count);
     void sendTestingData(int maxCount, String limitTime, int rgTestType, int rgTestTimeOption,String host,String title);
-    void sendTestResult(int maxCount, int trueCount, String[] myAnswer,String[] answer);
+    void sendTestResult(int maxCount, int trueCount, String[] myAnswer, String[] answer, ArrayList<DictionaryWordItem> list);
 }
