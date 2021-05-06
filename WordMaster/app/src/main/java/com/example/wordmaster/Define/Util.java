3b78a7b17b9a1551ec6e4751689e16835335be7a
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-05-06 09:43:31 +0900

    파베 관련 util 함수 사용

--- WordMaster/app/src/main/java/com/example/wordmaster/Define/Util.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/Define/Util.java
@@ -1,4 +1,12 @@
 package com.example.wordmaster.Define;
 
-public class util {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Util {
+    public static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public static DatabaseReference myRefUser = database.getReference(Define.FIREBASE_REFERENCE_USER_ACCOUNT);
+    public static DatabaseReference myRefWord = database.getReference();
+
+
 }
