8b6e5f529d730d39c137927bcd33a3fbd50a7d72
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-05-09 18:27:56 +0900

    바텀시트에서 넘길 콜백 변경 및 추가

--- WordMaster/app/src/main/java/com/example/wordmaster/callback/BottomSheetCallBack.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/callback/BottomSheetCallBack.java
@@ -1,5 +1,6 @@
 package com.example.wordmaster.callback;
 
 public interface BottomSheetCallBack {
-    void createDialogGetData(String title,int count,int currentCount,String description,String hashTag,String DictOption);
+    void createDialogGetData(String title,int count,int currentCount,String description,
+                             String hashTag,String DictOption,String password);
 }
