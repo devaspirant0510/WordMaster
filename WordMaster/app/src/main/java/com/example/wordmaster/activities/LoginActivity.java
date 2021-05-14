b0f42680b45f26ef6bc3e226e2541648e9cd7a7c
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-05-14 21:07:04 +0900

    코드 리팩토링 및 정리 주석추가

--- WordMaster/app/src/main/java/com/example/wordmaster/activities/LoginActivity.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/activities/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.wordmaster.activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -13,12 +12,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.wordmaster.model.firebase.UserAccount;
+import com.example.wordmaster.Define.Const;
+import com.example.wordmaster.Define.SharedManger;
+import com.example.wordmaster.Define.Util;
 import com.example.wordmaster.databinding.ActivityLoginBinding;
 import com.example.wordmaster.dialog.bottomsheet.LoginSuccessSheetDialog;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.example.wordmaster.model.firebase.UserAccount;
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
@@ -31,101 +30,31 @@ import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding mb;
-    private FirebaseAuth mAuth;
-    private static final int RESULT_SIGN_GOOGLE = 100;
     private static final String TAG = "LoginActivity";
-    private SharedPreferences sharedPreferences;
-    public static String ID = "";
-    public static String USER = "";
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mb = ActivityLoginBinding.inflate(getLayoutInflater());
+        // 컨텍스트를 넘겨서 sharedManger 에 저장
+        SharedManger.init(getApplicationContext());
         View root = mb.getRoot();
-        SharedPreferences sharedPreferences = getSharedPreferences("LoginInformation",MODE_PRIVATE);
-        if (sharedPreferences.getString("userId", "").equals("")){
+        // 처음 앱을 설치해서 로그인을 해야될경우 Login 화면 보여줌
+        Log.e(TAG, "onCreate: "+SharedManger.getUserId() );
+        if (SharedManger.getUserId().equals("")){
             setContentView(root);
             //getHashKey();
-            setFirebase();
             init();
 
         }
+        // 로그인 한적이 있을떄 바로 메인화면으로 전환
         else{
             changeLogin2Main();
         }
 
     }
-
-    private void setFirebase() {
-        mDatabase = FirebaseDatabase.getInstance();
-        mRef = mDatabase.getReference("UserAccount");
-
-    }
-    private void addUserInformationFirebase(String id, UserAccount account){
-        Log.e(TAG, "addUserInformationFirebase: " );
-        mRef.child(id).setValue(null);
-        mRef.child(id).setValue(account);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    private void changeLogin2Main(){
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        startActivity(intent);
-        finish();
-
-    }
-    private void showLoginSuccessDialog(){
-
-        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-            @Override
-            public Unit invoke(User user, Throwable throwable) {
-                if (user != null) {
-                    USER = String.valueOf(user.getId());
-
-                    LoginSuccessSheetDialog loginSuccessSheetDialog = new LoginSuccessSheetDialog();
-                    Bundle bundle = new Bundle();
-                    bundle.putString("image_uri",user.getKakaoAccount().getProfile().getProfileImageUrl());
-                    bundle.putString("nickname",user.getKakaoAccount().getProfile().getNickname());
-                    loginSuccessSheetDialog.setArguments(bundle);
-                    loginSuccessSheetDialog.show(getSupportFragmentManager(),"login_success_bottom_sheet");
-                    loginSuccessSheetDialog.setBottomSheetSetOnClick(new LoginSuccessSheetDialog.BottomSheetSetOnClick() {
-                        @Override
-                        public void clickSubmit(String name) {
-                            //Log.e(TAG, user.getKakaoAccount().getAgeRange().toString()+"" );
-                            addUserInformationFirebase(String.valueOf(user.getId()),new UserAccount(
-                                    name,
-                                    user.getKakaoAccount().getEmail(),
-                                    user.getKakaoAccount().getGender().toString(),
-                                    "",
-                                    user.getKakaoAccount().getBirthday(),
-                                    user.getKakaoAccount().getProfile().getProfileImageUrl()
-                            ));
-                            SharedPreferences sharedPreferences = getSharedPreferences("LoginInformation",MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString("userId",String.valueOf(user.getId()));
-                            editor.putString("userNickName",name);
-                            editor.putString("userEmail",user.getKakaoAccount().getEmail());
-                            editor.apply();
-                            changeLogin2Main();
-                        }
-                    });
-
-                } else {
-
-                }
-                return null;
-            }
-        });
-    }
     public void init() {
+        // 로그인 성공여부 체크 콜백 메서드
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
             @Override
             public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
@@ -137,35 +66,87 @@ public class LoginActivity extends AppCompatActivity {
                 return null;
             }
         };
+        // 카카오톡 설치여부에따라 자동로그인을 해주거나 웹에서 로그인
         mb.kakaoLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
                     UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this, callback);
-                    showLoginSuccessDialog();
 
                 } else {
                     UserApiClient.getInstance().loginWithKakaoAccount(LoginActivity.this, callback);
-                    showLoginSuccessDialog();
 
                 }
+                showLoginSuccessDialog();
             }
         });
 
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(TAG, "onStop: ");
+    /**
+     * 처음 가입할경우 파베에 유저정보 저장 키값과 밸류값 받음
+     * @param id 키값 : 유저 아이디(계정당 가질수 있는 교유값을 키값으로 지정)
+     * @param account 밸류값 : 계정정보를 저장한 객체를 밸류값으로 지정
+     */
+    private void addUserInformationFirebase(String id, UserAccount account){
+        Util.myRefUser.child(id).setValue(account);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e(TAG, "onDestroy: ");
+    // 로그인 화면에서 메인 화면으로 전환
+    private void changeLogin2Main(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+        finish();
+
+    }
+    // 카카오 로그인 성공시
+    private void showLoginSuccessDialog(){
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                // 유저정보를 가져왔을때
+                if (user != null) {
+                    // 유저 고유 값
+                    String userId = String.valueOf(user.getId());
+                    // 로그인 성공시 유저정보 바텀시트 뿌려줄 다이얼로그
+                    LoginSuccessSheetDialog loginSuccessSheetDialog = new LoginSuccessSheetDialog();
+                    // 다이얼로그에 넘겨줄값 번들에 저장
+                    Bundle bundle = new Bundle();
+                    // 프사 uri 와 닉네임 넘겨줌
+                    bundle.putString("image_uri",user.getKakaoAccount().getProfile().getProfileImageUrl());
+                    bundle.putString("nickname",user.getKakaoAccount().getProfile().getNickname());
+                    loginSuccessSheetDialog.setArguments(bundle);
+                    loginSuccessSheetDialog.show(getSupportFragmentManager(),"login_success_bottom_sheet");
+                    loginSuccessSheetDialog.setBottomSheetSetOnClick(new LoginSuccessSheetDialog.BottomSheetSetOnClick() {
+                        // 다이얼로그에서 콜백 받아서 name 값 가져옴
+                        @Override
+                        public void clickSubmit(String name) {
+                            //Log.e(TAG, user.getKakaoAccount().getAgeRange().toString()+"" );
+                            addUserInformationFirebase(String.valueOf(user.getId()),new UserAccount(
+                                    name,
+                                    user.getKakaoAccount().getEmail(),
+                                    user.getKakaoAccount().getGender().toString(),
+                                    "",
+                                    user.getKakaoAccount().getBirthday(),
+                                    user.getKakaoAccount().getProfile().getProfileImageUrl()
+                            ));
+                            // 셰어드 프리퍼런스에 계정 정보 저장
+                            SharedManger.saveUserId(Const.SHARED_USER_ID, userId);
+                            SharedManger.saveUserName(Const.SHARED_USER_NAME,name);
+                            SharedManger.saveUserProfileUri(Const.SHARED_USER_PROFILE_URI,user.getKakaoAccount().getProfile().getProfileImageUrl());
+                            SharedManger.saveUserEmail(Const.SHARED_USER_EMAIL,user.getKakaoAccount().getEmail());
+                            changeLogin2Main();
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(),"유저정보를 제대로 가져오지 못헀습니다. 다시시도해주세요",Toast.LENGTH_LONG).show();
+                }
+                return null;
+            }
+        });
     }
 
+    // 해시키  가져오기
     private void getHashKey() {
         PackageInfo packageInfo = null;
         try {
