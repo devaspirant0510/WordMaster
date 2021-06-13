9d07b45e0e8819ffbfd6e9453c039063653bc4e2
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-13 20:16:23 +0900

    프로필사진 원형 처리, 활동기록기능 추가

--- WordMaster/app/src/main/java/com/example/wordmaster/fragment/ProfileFragment.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/fragment/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.wordmaster.fragment;
 
 import android.content.Context;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +11,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.wordmaster.Define.Const;
 import com.example.wordmaster.Define.SharedManger;
+import com.example.wordmaster.adapter.ActivityBoardAdapter;
 import com.example.wordmaster.databinding.FragmentProfileBinding;
 
 import org.jetbrains.annotations.NotNull;
@@ -20,21 +25,43 @@ import org.jetbrains.annotations.NotNull;
 public class ProfileFragment extends Fragment {
     private FragmentProfileBinding mb;
     private String profileURI;
+    private ActivityBoardAdapter adapter;
     @Override
     public void onAttach(@NonNull @NotNull Context context) {
         super.onAttach(context);
     }
 
     @Override
-    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable  Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mb = FragmentProfileBinding.inflate(getLayoutInflater());
         profileURI = SharedManger.loadData(Const.SHARED_USER_PROFILE_URI,"");
+        mb.ivProfile.setBackground(new ShapeDrawable(new OvalShape()));
+        mb.ivProfile.setClipToOutline(true);
+
         Glide.with(getContext()).load(profileURI).into(mb.ivProfile);
+        init();
     }
 
+    private void init(){
+        GridLayoutManager gm = new GridLayoutManager(getContext(),7, RecyclerView.VERTICAL,false);
+        mb.gridRvActivityBoard.setLayoutManager(gm);
+        adapter = new ActivityBoardAdapter();
+        adapter.addItem(0);
+        adapter.addItem(3);
+        adapter.addItem(0);
+        adapter.addItem(3);
+        adapter.addItem(0);
+        adapter.addItem(3);
+        adapter.addItem(0);
+        adapter.addItem(3);
+        mb.gridRvActivityBoard.setAdapter(adapter);
+
+
+    }
+
+
     @Nullable
-    @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         return mb.getRoot();
