e12f4b0ea7e60a284d471b4d80094ec1b82e9c80
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-15 18:57:52 +0900

    바텀시트 다이얼로그로 변경, 리사이클러뷰 아이템에서 넘긴값 보여주기

--- WordMaster/app/src/main/java/com/example/wordmaster/dialog/bottomsheet/SearchInfoSheetDialog.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/dialog/bottomsheet/SearchInfoSheetDialog.java
@@ -1,5 +1,7 @@
 package com.example.wordmaster.dialog.bottomsheet;
 
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,31 +10,79 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 
 import com.bumptech.glide.Glide;
+import com.example.wordmaster.Define.GlideChip;
+import com.example.wordmaster.Define.Util;
+import com.example.wordmaster.R;
 import com.example.wordmaster.databinding.DialogBottomSheetFragmentSearchInfoBinding;
+import com.example.wordmaster.model.firebase.UserAccount;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
-public class SearchInfoSheetDialog extends DialogFragment {
+import org.jetbrains.annotations.NotNull;
+
+public class SearchInfoSheetDialog extends BottomSheetDialogFragment {
     private DialogBottomSheetFragmentSearchInfoBinding mb;
     private int pos;
-    private String user;
+    private String user,profileURI;
     private static final String TAG = "SearchInfoSheetDialog";
+    private String userId,description,host,title,userName;
+
+    public SearchInfoSheetDialog(String userId) {
+        this.userId = userId;
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
         mb = DialogBottomSheetFragmentSearchInfoBinding.inflate(getLayoutInflater());
         Bundle bundle = getArguments();
         if (bundle != null) {
             user = bundle.getString("user");
             pos = bundle.getInt("pos");
+            description = bundle.getString("description");
+            host = bundle.getString("host");
+            title = bundle.getString("title");
+            userName = bundle.getString("userName");
         }
         Log.e(TAG, "onCreate: "+user );
+        getProfileURI(userId);
+        mb.tvSearchInfoHost.setText(host);
+        mb.tvSearchInfoDescription.setText(description);
         mb.tvSearchInfoHost.setText(user);
+        mb.tvSearchInfoTitle.setText(title);
+    }
+    private void getProfileURI(String id){
+        Util.myRefUser.child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                UserAccount userAccount = snapshot.getValue(UserAccount.class);
+                if (userAccount!=null){
+                    profileURI= userAccount.getUserProfileUri();
+                    Log.e(TAG, "onDataChange: "+profileURI );
+                    mb.imageView.setBackground(new ShapeDrawable(new OvalShape()));
+                    mb.imageView.setClipToOutline(true);
+                    Glide.with(getContext()).load(profileURI).into(mb.imageView);
+                    GlideChip chip = new GlideChip(getContext());
+                    chip.setIconUrl(profileURI,null);
+                    chip.setText(userName);
+                    chip.setChipIconVisible(true);
+                    mb.rgUserGroup.addView(chip);
 
 
+                }
 
+            }
 
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Nullable
@@ -43,7 +93,6 @@ public class SearchInfoSheetDialog extends DialogFragment {
 
     }
     private void init(){
-        Glide.with(this).load("https://k.kakaocdn.net/dn/ifxIf/btq4ehKeRw2/jq820wk55NSiLlKAsbYOy0/img_640x640.jpg").into(mb.imageView);
 
 
     }
