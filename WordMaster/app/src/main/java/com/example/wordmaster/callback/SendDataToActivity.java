2e83c3971e268b51d01e339721a5941a49e769ea
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-16 19:23:31 +0900

    콜백함수 추가 searchFragment 에서SearchInfoFragment로 트랜잭션하기

--- WordMaster/app/src/main/java/com/example/wordmaster/callback/SendDataToActivity.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/callback/SendDataToActivity.java
@@ -1,5 +1,7 @@
 package com.example.wordmaster.callback;
 
+import android.os.Bundle;
+
 import com.example.wordmaster.model.recycler.DictionaryListItem;
 import com.example.wordmaster.model.recycler.DictionaryWordItem;
 
@@ -13,4 +15,6 @@ public interface SendDataToActivity {
     void sendTestingData(String userid,String roomKey,int maxCount,int rgTestType,String host,String title);
 
     void sendTestResult(int maxCount, int trueCount, String[] myAnswer, String[] answer, ArrayList<DictionaryWordItem> list);
+
+    void sendSearchInfoData(Bundle bundle);
 }
