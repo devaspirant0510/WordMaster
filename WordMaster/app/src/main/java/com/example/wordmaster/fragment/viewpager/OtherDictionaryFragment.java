bde477e57624e46fc2d1fc8af590ec1bb790f63b
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-21 00:37:48 +0900

    외부 단어장 불러오고 아이템 클릭했을때 상세화면으로 트랜잭션

--- WordMaster/app/src/main/java/com/example/wordmaster/fragment/viewpager/OtherDictionaryFragment.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/fragment/viewpager/OtherDictionaryFragment.java
@@ -1,5 +1,6 @@
 package com.example.wordmaster.fragment.viewpager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,7 +14,10 @@ import androidx.fragment.app.Fragment;
 import com.example.wordmaster.Define.Const;
 import com.example.wordmaster.Define.SharedManger;
 import com.example.wordmaster.Define.Util;
+import com.example.wordmaster.activities.MainActivity;
 import com.example.wordmaster.adapter.DictionaryListAdapter;
+import com.example.wordmaster.callback.DictionaryListCallBack;
+import com.example.wordmaster.callback.SendDataToActivity;
 import com.example.wordmaster.databinding.FragmentOtherDictionaryBinding;
 import com.example.wordmaster.model.firebase.UserDictionary;
 import com.example.wordmaster.model.recycler.DictionaryListItem;
@@ -29,8 +33,19 @@ import java.util.HashMap;
 public class OtherDictionaryFragment extends Fragment {
     private static final String TAG = "OtherDictionaryFragment";
     private FragmentOtherDictionaryBinding mb;
+    private MainActivity activity;
     private DictionaryListAdapter adapter;
+    private SendDataToActivity callback = null;
 
+
+    @Override
+    public void onAttach(@NonNull @NotNull Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) getActivity();
+        if (context instanceof SendDataToActivity) {
+            callback = (SendDataToActivity) context;
+        }
+    }
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -48,27 +63,53 @@ public class OtherDictionaryFragment extends Fragment {
 
         return mb.getRoot();
     }
-    private void init(){
-        adapter = new DictionaryListAdapter(getContext(),false);
+
+    private void init() {
+        adapter = new DictionaryListAdapter(getContext(), false);
+        adapter.setDictionaryListCallBack(new DictionaryListCallBack() {
+            @Override
+            public void onClick(View v, int pos) {
+                DictionaryListItem item = adapter.getItem(pos);
+
+
+                callback.otherDict2Info(item.getDictionaryTitle(),
+                        item.getDictOption(),
+                        Integer.parseInt(item.getDictionaryMaxCount()),
+                        item.getDictRoomKey(),
+                        item.getUserId(),
+                        item.getDictionaryHost());
+                activity.changeFragment(Const.OTHER_DICT2DICTIONARY_INFO);
+
+                Log.e(TAG, "onClick:" + pos);
+
+            }
+
+            @Override
+            public void onLongClick(View v, int pos) {
+                Log.e(TAG, "onLongClick: " + pos);
+
+            }
+        });
         mb.otherDictList.setAdapter(adapter);
     }
-    private void readOtherWordList(){
-        String userId = SharedManger.loadData(Const.SHARED_USER_ID,"");
+
+    private void readOtherWordList() {
+        String userId = SharedManger.loadData(Const.SHARED_USER_ID, "");
         Util.myRefUser.child(userId).child("userDownloadDict").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                Log.e(TAG, "onDataChange:"+snapshot );
-                HashMap<String, ArrayList<String>> hashMap = (HashMap<String,ArrayList<String>>)snapshot.getValue();
-                if(hashMap!=null){
-                    Log.e(TAG, "onDataChange: "+hashMap.keySet() );
+                Log.e(TAG, "onDataChange:" + snapshot);
+                HashMap<String, ArrayList<String>> hashMap = (HashMap<String, ArrayList<String>>) snapshot.getValue();
+                if (hashMap != null) {
+                    Log.e(TAG, "onDataChange: " + hashMap.keySet());
 
-                    for(String key:hashMap.keySet()){
-                        for(String value:hashMap.get(key)){
+                    for (String key : hashMap.keySet()) {
+                        for (String value : hashMap.get(key)) {
                             Util.myRefWord.child(key).child(value).addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
                                     UserDictionary item = snapshot.getValue(UserDictionary.class);
-                                    if (item!=null){
+                                    if (item != null) {
                                         adapter.addItem(new DictionaryListItem(
                                                 item.getTitle(),
                                                 String.valueOf(item.getMaxCount()),
@@ -77,10 +118,12 @@ public class OtherDictionaryFragment extends Fragment {
                                                 item.getOption(),
                                                 item.getRoomKey(),
                                                 item.getHashTag(),
+                                                item.getHostId(),
+                                                item.getHost(),
                                                 1
 
-                                                ));
-                                        adapter.notifyItemInserted(adapter.getItemCount()-1);
+                                        ));
+                                        adapter.notifyItemInserted(adapter.getItemCount() - 1);
                                     }
 
                                 }
@@ -90,14 +133,14 @@ public class OtherDictionaryFragment extends Fragment {
 
                                 }
                             });
-                            Log.e(TAG, "onDataChange: "+key+" "+value);
+                            Log.e(TAG, "onDataChange: " + key + " " + value);
 
                         }
 
                     }
 
 
-                    Log.e(TAG, "onDataChange: "+hashMap.values() );
+                    Log.e(TAG, "onDataChange: " + hashMap.values());
                 }
             }
 
