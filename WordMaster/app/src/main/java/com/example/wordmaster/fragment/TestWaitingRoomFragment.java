d3667f06c1826c744c0479dcd4604a4b92bdeda3
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-27 16:01:50 +0900

    테스트 대기실 프레그먼트에서 단어정보 가져오기

--- WordMaster/app/src/main/java/com/example/wordmaster/fragment/TestWaitingRoomFragment.java
+++ WordMaster/app/src/main/java/com/example/wordmaster/fragment/TestWaitingRoomFragment.java
@@ -1,6 +1,7 @@
 package com.example.wordmaster.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.wordmaster.Define.Util;
+import com.example.wordmaster.adapter.DictionaryInfoAdapter;
 import com.example.wordmaster.databinding.FragmentTestWaitingRoomBinding;
+import com.example.wordmaster.model.recycler.DictionaryWordItem;
+import com.example.wordmaster.model.recycler.OnlineTestItem;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -25,10 +33,60 @@ import org.jetbrains.annotations.NotNull;
 public class TestWaitingRoomFragment extends Fragment {
     private static String TAG = "TestWaitingRoomFragment";
     private FragmentTestWaitingRoomBinding mb;
+    private DictionaryInfoAdapter adapter;
+    private String roomKey,userId;
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        adapter = new DictionaryInfoAdapter(getContext(),true);
+        mb.rvTestWaitingDictPreview.setAdapter(adapter);
+        readDB();
+
+
+    }
+    private void readDB(){
+        Util.myRefWord.child(userId).child(roomKey).child("list").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                Log.e(TAG, "onDataChange: "+snapshot.getValue() );
+                for (DataSnapshot item :snapshot.getChildren() ) {
+                    DictionaryWordItem getItem = item.getValue(DictionaryWordItem.class);
+                    if (getItem!=null){
+                        DictionaryWordItem wordItem = new DictionaryWordItem(
+                                getItem.getKor(),
+                                getItem.getEng()
+                        );
+                        wordItem.setIndex(adapter.getItemCount()+1);
+                        adapter.addItem(wordItem);
+                        adapter.notifyItemInserted(adapter.getItemCount()-1);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+    }
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mb = FragmentTestWaitingRoomBinding.inflate(getLayoutInflater());
+        Bundle bundle = getArguments();
+        if (bundle!=null){
+            OnlineTestItem item = (OnlineTestItem)bundle.getSerializable("item");
+            Log.e(TAG, "onCreate: "+item.getTitle() );
+            roomKey = item.getRoomKey();
+            userId = item.getHostId();
+            mb.tvTestRoomTitle.setText(item.getTitle());
+            mb.tvTimeLeft.setText(item.getEndTime());
+
+        }
     }
 
     @Nullable
