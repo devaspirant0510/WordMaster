d7747a6b14da27f2832aa070735b0c2bce5f5776
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-11-11 18:21:03 +0900

    뷰모델 연결, 유저데이터 불러오고 리사이클러뷰에 뿌려주기

--- WordMaster/app/src/main/java/dev/seh/wordmaster/ui/fragment/main/HomeFragment.kt
+++ WordMaster/app/src/main/java/dev/seh/wordmaster/ui/fragment/main/HomeFragment.kt
@@ -1,18 +1,39 @@
 package dev.seh.wordmaster.ui.fragment.main
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import dev.seh.wordmaster.R
+import dev.seh.wordmaster.UserRankingAdapter
+import dev.seh.wordmaster.base.BaseFragment
+import dev.seh.wordmaster.databinding.FragmentHomeBinding
+import dev.seh.wordmaster.repository.Repository
+import dev.seh.wordmaster.viewmodel.HomeFragmentViewModel
 
-class HomeFragment :Fragment(){
-/*
-    private val mBinding by lazy{
-        FragmentHomeBinding.inflate(layoutInflater)
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private lateinit var repository: Repository
+    private lateinit var homeViewModel: HomeFragmentViewModel
+
+    companion object {
+        private const val TAG = "HomeFragment"
     }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return mBinding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        homeViewModel = ViewModelProvider(requireActivity())
+            .get(HomeFragmentViewModel::class.java)
+        mBinding.viewmodel = homeViewModel
+        mBinding.rvUserRanking.adapter = UserRankingAdapter()
+        observeData()
     }
-*/
+
+    private fun observeData() {
+        homeViewModel.userData.observe(viewLifecycleOwner, {
+            Log.e(TAG, "observeData:${it.userName}")
+            mBinding.tvWelcomeMessage.text = it.userName
+
+        })
+
+    }
 }
