fb3f4736199c8b56db77a24ea4d7ca688c99de2a
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-11-11 18:19:17 +0900

    파베 데이터베이스 UserAccount dto

--- WordMaster/app/src/main/java/dev/seh/wordmaster/model/firebase/UserModel.kt
+++ WordMaster/app/src/main/java/dev/seh/wordmaster/model/firebase/UserModel.kt
@@ -1,3 +1,18 @@
 package dev.seh.wordmaster.model.firebase
 
+import com.google.firebase.database.IgnoreExtraProperties
+import java.util.*
 
+@IgnoreExtraProperties
+class UserModel(
+    var activityHistory: HashMap<String, HashMap<String, HashMap<String, Int>>>?=null,
+    var joinDate:String?=null,
+    var userAge:String?=null,
+    var userEmail:String?=null,
+    var userDownloadDict:HashMap<String,List<String>>?=null,
+    var userComment:String?=null,
+    var userGender:String?=null,
+    var userJoinTestId:String?=null,
+    var userName:String?=null,
+    var userProfileUri:String?=null
+)
