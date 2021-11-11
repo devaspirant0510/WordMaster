17890a7fdf926bc5a274faf4e516b70b8b69d32a
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-11-11 18:22:03 +0900

    뷰모델 연결

--- WordMaster/app/src/main/java/dev/seh/wordmaster/ui/fragment/main/ProfileFragment.kt
+++ WordMaster/app/src/main/java/dev/seh/wordmaster/ui/fragment/main/ProfileFragment.kt
@@ -1,18 +1,21 @@
 package dev.seh.wordmaster.ui.fragment.main
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import dev.seh.wordmaster.R
+import dev.seh.wordmaster.base.BaseFragment
+import dev.seh.wordmaster.databinding.FragmentProfileBinding
+import dev.seh.wordmaster.viewmodel.HomeFragmentViewModel
 
-class ProfileFragment :Fragment(){
-/*
-    private val mBinding by lazy {
-        FragmentProfileBinding.inflate(layoutInflater)
+
+class ProfileFragment :BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile){
+    private lateinit var viewModel:HomeFragmentViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(HomeFragmentViewModel::class.java)
+        mBinding.viewmodel = viewModel
+
+
     }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return mBinding.root
-    }
-*/
 }
